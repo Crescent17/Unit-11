5a7d4c0e3e1942358a0e896472263f5dde7d7f4e
Crescent17 <maratos2001@mail.ru>
2021-12-28 10:46:35 +0300

    commit

---
+++ JarTester/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Series.fibonacci(6));
+        System.out.println(Series.factorial(6));
+        System.out.println(Series.nSum(10));
+    }
+}
